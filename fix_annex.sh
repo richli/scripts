799ecf52062814744873d13c24044ecc8e0c97f9
Richard Lindsley <richli.ff@gmail.com>
2012-12-17 22:52:28 -0700

    Exit earlier if no annexed files found

--- fix_annex.sh
+++ fix_annex.sh
@@ -33,26 +33,32 @@ ANNEX=$(pwd)/.git/annex
 # Look for symbolic links that go into the annex
 files_raw=($(find $(pwd) -type l -lname '*.git/annex/*' -printf '%l\n'))
 
-# Trim link to get to annex (Otherwise an annex file that is subdirectories
-# below the root will have lots of extra ../../.. prepending it, which we
-# don't care about so discard here)
-files=${files_raw[@]##*/.git/annex/}
+if [[ "${#files[@]}" -eq 0 ]]; then
+    echo "No git annexed files found"
+else
+    # Trim link to get to annex (Otherwise an annex file that is subdirectories
+    # below the root will have lots of extra ../../.. prepending it, which we
+    # don't care about so discard here)
+    files=${files_raw[@]##*/.git/annex/}
 
-for file in "${files[@]}"; do
+    for file in "${files[@]}"; do
 
-    # Compute the three-character directory name
-    file_hash=$(echo -n ${file##*/} | md5sum)
-    hash_dir="${file_hash:0:3}/${file_hash:3:3}"
-    dest_file=${file/objects\/??\/??\//$hash_dir\/}
+        # Compute the three-character directory name
+        file_hash=$(echo -n ${file##*/} | md5sum)
+        hash_dir="${file_hash:0:3}/${file_hash:3:3}"
+        dest_file=${file/objects\/??\/??\//$hash_dir\/}
 
-    # Make directory and symlink if needed
-    if [[ -e $ANNEX/$file ]]; then
-        echo "2-character hash exists"
-    else
-        echo "2-character hash DOES NOT exist"
-        mkdir -p $ANNEX/${file%/*/*}
-        #echo "${file%/*} should point to ${dest_file%/*}"
-        ln -s $ANNEX/${dest_file%/*} $ANNEX/${file%/*}
-    fi
+        # Make directory and symlink if needed
+        echo $file
+        if [[ -e $ANNEX/$file ]]; then
+            echo "2-character hash exists"
+        else
+            echo "2-character hash DOES NOT exist"
+            mkdir -p $ANNEX/${file%/*/*}
+            #echo "${file%/*} should point to ${dest_file%/*}"
+            ln -s $ANNEX/${dest_file%/*} $ANNEX/${file%/*}
+        fi
 
-done
+    done
+
+fi
