af196e8cc6c6fb58e9a7469ba07639cd048a15b0
Richard Lindsley <richli.ff@gmail.com>
2012-12-17 22:52:13 -0700

    Keep it under 80 columns

--- fix_annex.sh
+++ fix_annex.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# For all the git annex objects with the 3-character directory hash, create links from 2-character directory hash to 3-character hash
+# For all the git annex objects with the 3-character directory hash, create
+# links from 2-character directory hash to 3-character hash
 
 # A proper git annex has files stored like this:
 # .git/annex/objects/ab/cd/key/key
@@ -8,10 +9,14 @@
 
 # A git annex directory or rsync special remote has files stored like this:
 # .git/annex/abc/def/key/key
-# where abc and def are three-character values generated differently from the key MD5
+# where abc and def are three-character values generated differently from the
+# key MD5
 
-# The problem is the files in the git repo are symlinks to the annex, but they use the two-character directory style, whereas if the files are stored via rsync, they're in the three-character directory style.
-# This script symlinks the files in the three-character directories to where they *should* be, in the two-character directories
+# The problem is the files in the git repo are symlinks to the annex, but they
+# use the two-character directory style, whereas if the files are stored via
+# rsync, they're in the three-character directory style.  This script symlinks
+# the files in the three-character directories to where they *should* be, in
+# the two-character directories
 
 # Helpful information from here: http://git-annex.branchable.com/bugs/using_old_remote_format_generates_irritating_output/
 
@@ -19,9 +24,10 @@
 
 # Created 17 Dec 2012 richli
 
+shopt -s nullglob
 echo "$0: Symlink git annex objects from three-character directories to two-character ones"
 
-# TODO: Find annex location by recursively searching up the directory tree
+# TODO: Find annex location by recursively searching up the directory tree?
 ANNEX=$(pwd)/.git/annex
 
 # Look for symbolic links that go into the annex
